fc1732b7ca607e372f4aab1b646d1e626d035f15
Johan <johan.aspenlind@b3.se>
2023-06-19 20:35:59 +0200

    bump deps

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,9 @@ import path, { concat, empty, root } from "./lib/path";
 import { exists, isEmpty, isRoot } from "./lib/path/meta";
 import { pathString } from "./lib/path/parse";
 import walker from "./lib/walker";
-// import { sameAs } from "./lib/walker/until/sameAs";
-// import { exists as untilExists } from "./lib/walker/until/exists";
 
 export * from "./types";
 export { until } from "./lib/walker/until";
-// export const until = {
-//   sameAs,
-//   exists: untilExists
-// };
 
 /** @category path */
 export const flexi: Flexi = {
